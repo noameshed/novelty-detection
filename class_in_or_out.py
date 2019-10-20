416129f8e12af1e96de209979599c99da31aa17a
Noam Eshed <noam_eshed@yahoo.com>
2019-10-20 11:27:40 -0400

    updated in/out class file

--- class_in_or_out.py
+++ class_in_or_out.py
@@ -1,28 +1,30 @@
 import json
+import numpy as np
 import os
 import pandas as pd
 
-def create_label_csv(labels_dict):
 
-	df = pd.DataFrame(columns = ['index', 'id', 'name', 'in_inaturalist'])
 
-	for i in labels_dict:
-		imagenet_index = i
-		imagenet_id = labels_dict[i][0]
-		common_name = labels_dict[i][1]
-
-		df.loc[len(df)] = [imagenet_index, imagenet_id, common_name, False]
-
-	df.to_csv('in_out_classes.csv')
 
 if __name__ == '__main__':
 
-	# Load imagenet labels as dictionary
-	f = open('D:/noam_/Cornell/CS7999/imagenet_class_index.json', 'r')
-	imagenet_labels = json.load(f)
-
-	create_label_csv(imagenet_labels)
-
+	# Load dataframe of inaturalist annotations
+	df = pd.read_csv('in_out_class.csv')
 	
 
-		
+	# Extract only Aves that are labeled
+	birds = df[df['Biological Group']=='Aves']
+	birds = birds[birds['Annotator'].notnull()]
+	print(len(birds), ' total annotated birds')
+	
+	rels = birds[birds['Relation to Imagenet']=='relative in imagenet']
+	print(len(rels), ' with relatives in imagenet')
+
+	is_in = birds[birds['Relation to Imagenet']=='in imagenet']
+	print(len(is_in), ' in imagenet')
+
+	par = birds[birds['Relation to Imagenet']=='parent in imagenet']
+	print(len(par), ' parent in imagenet')
+
+	not_in = birds[birds['Relation to Imagenet']=='not in imagenet']
+	print(len(not_in), ' not in imagenet')
--- game.md
+++ game.md
@@ -6,7 +6,7 @@ Sample image:
 
 
 Sample textbox:
-<form action="/action_page.txt">
+<form action="/action_page.php">
 	First name: <input type="text" name="fname"><br>
 	Last name: <input type="text" name="lname"><br>
 	<input type="submit" value="Submit">
