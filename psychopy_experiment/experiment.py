d3b13c9ecd6b9a4aa72dde30ccaf7ccc743defb2
Noam Eshed <noam_eshed@yahoo.com>
2020-02-14 14:36:31 -0500

    added practice questions to experiment

--- psychopy_experiment/experiment.py
+++ psychopy_experiment/experiment.py
@@ -35,20 +35,20 @@ dataFile.write('leftIm,rightIm,userChoice\n')
 # create window
 win = visual.Window([1920, 1080],allowGUI=True, color=[0.2,0.2,0.2],
                     monitor='testMonitor', units='height')
-
+[x, y] = win.size
+print(x, y)
 # and some handy clocks to keep track of time
 globalClock = core.Clock()
 trialClock = core.Clock()
 
 # display instructions and wait
-msg1 = visual.TextStim(win, pos=[-.5,0.3], units='norm', height = 0.08, alignHoriz='left',
+msg1 = visual.TextStim(win, pos=[0.5, 0.1], height = 0.03,
     text="You will be shown two images. \n\n\
 Choose a value (1-8) on the scale to describe how similar they are.\n\n\
 1 is least similar and 8 is most similar. \n\n\
 Use the 1-8 keyboard keys to select the value. Then press Enter.")
 
-msg2 = visual.TextStim(win, pos=[-.5, -0.3],units='norm', height=0.08, alignHoriz='left',
-	text='Hit any key when you are ready to begin.')
+msg2 = visual.TextStim(win, pos=[0.5, -0.2], height=0.03, text='Press any key when you are ready to begin.')
 
 msg1.draw()
 msg2.draw()
@@ -57,8 +57,64 @@ win.flip()  #to show the messages
 #pause until there's a keypress
 event.waitKeys()
 
+
+# Run a few practice rounds for calibration
+msg1 = visual.TextStim(win, pos=[.5, 0.1], height=0.03,
+    text="You will now do a practice round. You will see pairs of images and choose a value (1-8) to decide how similar they are.\n\n\
+Press any key when you are ready to begin.")
+
+msg1.draw()
+win.flip()
+event.waitKeys()
+
+# Initialize path for bird images
 impath = path + '/images/'
 all_birds = os.listdir(impath)
+
+# Define the scale parameters
+msg = visual.TextStim(win, pos=[0.5, 0.3], height=0.02, alignHoriz='center',
+        text="Rate how similar the two images below are on a scale from 1-8. Then press Enter.")
+rating = visual.RatingScale(win=win,pos=[0, -.4], low=1, high=8, respKeys=['1','2','3','4','5','6','7','8'],
+        acceptKeys=['return', 'space'], showAccept=False, textColor=[0.2,0.2,0.2] )
+labels = visual.TextStim(win, pos=[0.72,-.22], height=0.02, alignHoriz='center',
+        text="1         2        3         4        5        6        7        8")
+least_sim = visual.TextStim(win, pos=[0.5, -.22], height=.02, text='very different')
+most_sim = visual.TextStim(win, pos=[1.35, -.22], height=.02, text='very similar')
+
+for i in range(10):
+    # choose 2 random images of 2 types of birds
+    [A, B] = random.sample(all_birds, 2)
+    choice1 = random.choice(os.listdir(impath+A))
+    choice2 = random.choice(os.listdir(impath+B))
+
+    leftIm = visual.ImageStim(win, image=impath+A+'/'+choice1, 
+        flipHoriz=True, pos=(-10,3), units='deg', size=(10,10))
+
+    rightIm = visual.ImageStim(win, image=impath+B+'/'+choice2, 
+        flipHoriz=True, pos=(10,3), units='deg', size=(10,10))    
+    rating.reset()
+
+    while rating.noResponse:
+        leftIm.draw()
+        rightIm.draw()
+        msg.draw()
+        rating.draw()
+        labels.draw()
+        least_sim.draw()
+        most_sim.draw()
+        win.flip()
+
+        if event.getKeys(['escape']):
+            core.quit()
+
+# Show messages telling the participant they are about to begin the real experiment
+msg1 = visual.TextStim(win, pos=[.5, 0.1], height=0.03,
+    text="You will now begin the experiment. You will see pairs of images and choose a value (1-8) to decide how similar they are.\n\n\
+Press any key when you are ready to begin.")
+
+msg1.draw()
+win.flip()
+event.waitKeys()
 
 for i in range(50): # will continue the staircase until it terminates!
     # choose 3 random images of 3 types of birds
@@ -72,22 +128,7 @@ for i in range(50): # will continue the staircase until it terminates!
     rightIm = visual.ImageStim(win, image=impath+B+'/'+choice2, 
         flipHoriz=True, pos=(10,3), units='deg', size=(10,10))
 
-    msg = visual.TextStim(win, pos=[0, 0.6], units='norm', height=0.06, alignHoriz='center',
-        text="Rate how similar the two images below are on a scale from 1-8. Then press Enter.")
-
-    #rating = visual.RatingScale(win=win, name='rating', marker='triangle', 
-     #   size=1.0, pos=[0.0, -0.4], low=1, high=8, labels=['similar', 'not similar'], scale='', markerStart='very similar')
-
-    rating = visual.RatingScale(win=win,pos=[0, -.4], low=1, high=8, respKeys=['1','2','3','4','5','6','7','8'],
-        acceptKeys=['return', 'space'], showAccept=False, textColor=[0.2,0.2,0.2] )
-
-    labels = visual.TextStim(win, pos=[0,-.45], units='norm', height=0.06, alignHoriz='center',
-        text="1        2       3        4       5       6       7       8")
-    
-    least_sim = visual.TextStim(win, pos=[-.5, -.45], units='norm',	height=.06, text='very different')
-    most_sim = visual.TextStim(win, pos=[.5, -.45], units='norm',	height=.06, text='very similar')
     rating.reset()
-
 
     while rating.noResponse:
         leftIm.draw()
