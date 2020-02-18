1a2611ecefe4a313358dba03806b1ea7d021769d
Noam Eshed <noam_eshed@yahoo.com>
2020-02-17 22:13:10 -0500

    split into bird prompt and image prompt

--- psychopy_experiment/experiment.py
+++ psychopy_experiment/experiment.py
@@ -8,17 +8,21 @@ import numpy, random, os, ctypes
 
 class BirdSimExp():
 
-    def __init__(self):
+    def __init__(self, prompt_type):
         self.name='Bird Similarity Experiment'
         self.expInfo = self.getSessionInfo()
+        if prompt_type == 0:
+            self.prompt = 'images'
+        else:
+            self.prompt = 'birds'
 
         # Initialize path for bird images
         self.path = os.getcwd()
         self.impath = self.path + '/images/'
         self.all_birds = os.listdir(self.impath)
 
-        # Make a text file to save data
-        fileName = self.expInfo['Participant ID'] + self.expInfo['dateStr']
+        # Make a new text file to save data
+        fileName = self.expInfo['Participant ID'] + '_PT=' + str(prompt_type) + '_' + self.expInfo['dateStr']
         self.dataFile = open(self.path+'/data/'+fileName+'.csv', 'w+')  # a simple text file with comma-separated-values
         self.dataFile.write('leftIm,rightIm,userChoice,responseTime\n')
 
@@ -30,7 +34,7 @@ class BirdSimExp():
 
         # Define the rating scale parameters
         self.scaleMsg = visual.TextStim(self.win, pos=[0,10], height=1, wrapWidth=40,           #[0.5, 0.3]
-                text="Rate how similar the two images below are on a scale from 1-7. Then press Enter.")
+                text="Rate how similar the two " + self.prompt + " below are on a scale from 1-7. Then press Enter.")
         self.rating = visual.RatingScale(self.win,pos=[0,-.3], low=1, high=7, respKeys=['1','2','3','4','5','6','7'],      #[0, -.4]
                 acceptKeys=['return', 'space'], showAccept=False, textColor=[0.2,0.2,0.2] )
         self.labels = visual.TextStim(self.win, pos=[0,-6], height=.8, wrapWidth=40,            # [0.72,-.22], 0.02
@@ -62,7 +66,7 @@ class BirdSimExp():
         # display instructions and wait
         msg = visual.TextStim(self.win, pos=[0, 0], height=1, wrapWidth=40,                 # [0.5, 0.1]    0.03
             text=
-            "In this experiment, you will be shown pairs of images.\n\n\
+            "In this experiment, you will be shown pairs of "+self.prompt+".\n\n\
 Choose a value (1-7) on the scale to describe how similar they are.\n\n\
 1 is least similar and 7 is most similar. Use the 1-7 keyboard keys to select the value.\n\n\
 Then press Enter. Press any key when you are ready to begin.")
@@ -72,15 +76,18 @@ Then press Enter. Press any key when you are ready to begin.")
         event.waitKeys()    # pause until there's a keypress
 
     def trials(self, n, writeData):
-        # Show messages telling the participant they are about to begin the real experiment
-        msg1 = visual.TextStim(self.win, pos=[0,0], height=1, wrapWidth=40,      # pos=[.5, 0.1], height=0.03,
-            text="You will now begin the experiment. You will see pairs of images and choose a value (1-7) to decide how similar they are.\n\nPress any key when you are ready to begin.")
-
-        msg1.draw()
+        # Show message telling the participant they are starting a warmup trial
+        msg = visual.TextStim(self.win, pos=[0,0], height=1, wrapWidth=40,
+                text="You will now begin a practice round. You will see pairs of images and choose a value (1-7) to decide how similar they are.\n\nPress any key when you are ready to begin.")
+        if writeData:   # Show messages telling the participant they are about to begin the real experiment
+            msg = visual.TextStim(self.win, pos=[0,0], height=1, wrapWidth=40,    
+                text="You will now begin the experiment. You will see pairs of images and choose a value (1-7) to decide how similar they are.\n\nPress any key when you are ready to begin.")
+            
+        msg.draw()
         self.win.flip()
         event.waitKeys()
 
-        # Initialize the timer 
+        # Initialize the response timer 
         timer = core.Clock()
 
         for i in range(n): # 200 birds is about 10 minutes
@@ -136,10 +143,16 @@ Then press Enter. Press any key when you are ready to begin.")
         core.quit()
 
 if __name__ == '__main__': 
-    b = BirdSimExp()
+
+    # Randomly select the participant's prompt type
+    prompt_type=0
+    if numpy.random.rand() < 0.5:
+        prompt_type=1
+
+    b = BirdSimExp(prompt_type)
     b.instructions()
-    b.trials(1, False)     # Run warmup trials (not recorded)
-    b.trials(1, True)     # Run experiment trials (recorded)
+    b.trials(15, False)     # Run warmup trials (not recorded)
+    b.trials(200, True)     # Run experiment trials (recorded)
     b.thankyou()
 
     # Survey user's familiarity with birds
